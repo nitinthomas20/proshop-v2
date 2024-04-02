c523a620091d60def7ab81a293614c4f9dd48165
nitinthomas20 <nitinthomas20@gmail.com>
2024-04-02 12:04:04 +1100

    Added product images and outline

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+   <>
+   <Header/>
+   <main className='py-3'>
+    <Container>
+      <HomeScreen/>
+    </Container>
+   </main>
+   <Footer />
+   </> 
   )
 }
 
